428794f741f85d014ad294bdb90bbee42c75e462
McxmCxmcX <414550551@qq.com>
2017-04-11 15:01:40 +0800

    update

--- src/main/java/collection/MyLinkedList.java
+++ src/main/java/collection/MyLinkedList.java
@@ -1,10 +1,5 @@
 package collection;
 
-import com.sun.prism.shader.Solid_TextureFirstPassLCD_AlphaTest_Loader;
-
-import javax.jws.soap.SOAPMessageHandler;
-import java.security.acl.LastOwnerException;
-
 /**
  * Created by heishuai.china.cn
  * 2017/4/11 10:28
@@ -12,29 +7,106 @@ import java.security.acl.LastOwnerException;
  */
 public class MyLinkedList {
 
-
     private Node first;
     private Node last;
+    private int size;
 
     public MyLinkedList() {
     }
 
     public static void main(String[] args) {
-        MyLinkedList myLinkedList = new MyLinkedList();
+
+                MyLinkedList myLinkedList = new MyLinkedList();
         myLinkedList.add("a");
         myLinkedList.add("b");
         myLinkedList.add("c");
-        System.out.println(myLinkedList.first());
-        System.out.println(myLinkedList.last());
-        System.out.println(myLinkedList.get(1));
+                System.out.println(myLinkedList.first());
+                System.out.println(myLinkedList.last());
+                System.out.println(myLinkedList.get(1));
+                System.out.println(myLinkedList.size());
+                myLinkedList.remove(0); // [a, c]
+                System.out.println(myLinkedList.size()); // 2
+                System.out.println(myLinkedList.get(0)); // c
+        //        System.out.println(myLinkedList.first());
+                //        System.out.println(myLinkedList.last());
+                        //        System.out.println(myLinkedList.get(1));
+                                //        System.out.println(myLinkedList.size());
+                                        //        myLinkedList.remove(1);
+                                                //        myLinkedList.remove(1);
+                                                        //        myLinkedList.remove(0);
+                                                                        System.out.println(myLinkedList); // c
     }
-/*
-   ----------[0]---[?]---[?]---[?].............[n?]
- */
+
+    public String remove(int index) {
+        Node node = getNode(index);
+        String element = node.element;
+        Node prev = node.prev;
+        Node next = node.next;
+        if (prev == null) {
+            next = first;
+            if (prev == null && next == null) {
+                node.element = null;
+            } else if (prev == null) {
+                first = next;
+                next.prev = null;
+                node.next = null;
+            }
+            if (next == null) {
+                prev = last;
+                node.element = null;
+            } else if (next == null) {
+                last = prev;
+                prev.next = null;
+                node.prev = null;
+                node.element = null;
+            } else {
+                prev.next = next;
+                next.prev = prev;
+                node.prev = null;
+                node.next = null;
+                node.element = null;
+            }
+            node.element = null;
+            size--;
+            return element;
+        }
+        return element;
+    }
+  /*
+     [0]---[1]---[2]---[?].............[n?]
+   */
+
+        public String toString() {
+                    String s = "[";
+                    for (int i = 0; i < size; i++) {
+                            String element = get(i);
+                            s += element;
+                            if (i != size - 1) {
+                                    s += ", ";
+                                }
+                        }
+                    s += "]";
+                    return s;
+                }
+
+        public int size() {
+            return size;
+        }
+
+    private Node getNode(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("error.");
+            System.exit(0);
+        }
+        Node node = first;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
 
     public String get(int index) {
-        // TODO: 4/11/17
-        return null;
+        return getNode(index).element;
     }
 
     public boolean add(String element) {
@@ -47,6 +119,7 @@ public class MyLinkedList {
             last.next = node;
             last = node;
         }
+        size++;
         return true;
     }
 
