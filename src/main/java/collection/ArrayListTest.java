80e51b6686b8c87ff210367f8eef6f8f05114db8
McxmCxmcX <414550551@qq.com>
2017-04-11 09:57:39 +0800

    update

--- src/main/java/collection/ArrayListTest.java
+++ src/main/java/collection/ArrayListTest.java
@@ -8,26 +8,29 @@ import java.util.List;
  */
 public class ArrayListTest {
     public static void main(String[] args) {
-                List<String> strings1 = new ArrayList<>();
-                strings1.add("a");
-                strings1.add("b");
-                List<String> strings2 = new ArrayList<>();
-                strings2.add("a");
-                strings2.add("b");
-                strings2.add("c");
+        List<String> strings1 = new ArrayList<>();
+        strings1.add("a");
+        strings1.add("b");
+        List<String> strings2 = new ArrayList<>();
+        strings2.add("a");
+        strings2.add("b");
+        strings2.add("c");
         // [a, c]
-                        System.out.println(strings1.equals(strings2));
+        System.out.println(strings1.equals(strings2));
 
-                        List<String> strings3 = strings2.subList(1, 3);
-                System.out.println(strings3);
+        List<String> strings3 = new ArrayList<>(strings2.subList(1, 3));// view 视图
+        System.out.println(strings3);
 
-                        System.out.println(strings3.containsAll(strings1));
+        System.out.println(strings3.containsAll(strings1));
 
-                        strings2.removeAll(strings3);
-                System.out.println(strings2);
+        strings2.removeAll(strings3);
+        System.out.println(strings2);
 
-                        // retain 保留\ [rɪ'teɪn]
-                                strings3.retainAll(strings2);
-                System.out.println(strings3);
-            }
- }
+        // retain 保留\ [rɪ'teɪn]
+        strings3.retainAll(strings2); // [b, c] [a]
+                // concurrent 并发\ [kən'kʌr\(ə\)nt]
+                        // modification 修改\ [,mɒdɪfɪ'keɪʃ\(ə\)n]
+        strings3.retainAll(strings2); // [b, c] [a, b, c]
+        System.out.println(strings3);
+    }
+}
---
+++ src/main/java/collection/LinkedListTest.java
@@ -0,0 +1,25 @@
+package collection;
+import java.util.LinkedList;
+/**
+ * Created by heishuai.china.cn
+ * 2017/4/11 9:46
+ * JAVASE
+ */
+public class LinkedListTest {
+    // link 链接\ [lɪŋk]
+            public static void main(String[] args) {
+                LinkedList<String> strings = new LinkedList<>();
+                strings.add("a");
+                strings.add("b");
+
+                        System.out.println(strings.size());
+                System.out.println(strings.get(0));
+
+                        for (String string : strings) {
+                        System.out.println(string);
+                    }
+
+                        System.out.println(strings.getFirst());
+                System.out.println(strings.getLast());
+            }
+ }
