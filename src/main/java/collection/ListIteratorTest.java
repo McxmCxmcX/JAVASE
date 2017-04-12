cec5b4cf01a7ff4ed2cda3f202ccf02538f4c025
McxmCxmcX <414550551@qq.com>
2017-04-12 11:37:24 +0800

    update

---
+++ src/main/java/collection/ListIteratorTest.java
@@ -0,0 +1,29 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+/**
+ * Created by heishuai.china.cn
+ * 2017/4/12 11:34
+ * JAVASE
+ */
+public class ListIteratorTest {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("a");
+        strings.add("b");
+        strings.add("c");
+        strings.add("d");
+        strings.add("e");
+
+        ListIterator<String> listIterator = strings.listIterator();
+
+        // next
+        // prev
+        // index
+        // add
+        // set
+        // remove
+    }
+}
