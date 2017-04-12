30cc87f69b6dd1cdc4d5c53e0c6bd5e4ef0c4657
McxmCxmcX <414550551@qq.com>
2017-04-12 15:37:55 +0800

    update

---
+++ src/main/java/collection/LT.java
@@ -0,0 +1,31 @@
+package collection;
+import java.util.*;
+/**
+ * Created by heishuai.china.cn
+ * 2017/4/12 15:36
+ * JAVASE
+ */
+public class LT {
+    public static void main(String[] args) {
+                ArrayList<String> list = new ArrayList<>();
+                list.add("a");
+                list.add("b");
+                list.add("c");
+                System.out.println(list);
+        //        Collections.reverse(list);
+                        System.out.println(list);
+        //        Collections.sort(list); // sort 排序\ [sɔːt]
+                        System.out.println(list);
+
+                        System.out.println(Collections.binarySearch(list, "c")); // ?
+
+                        Map<Integer, String> map = new HashMap<>();
+                map.put(1, "a");
+                map.put(2, "b");
+                map.put(3, "c");
+
+                        System.out.println(map); //
+
+                        System.out.println(Collections.singletonList(map)); // ?
+            }
+ }
--- src/main/java/collection/ListIteratorTest.java
+++ src/main/java/collection/ListIteratorTest.java
@@ -27,3 +27,4 @@ public class ListIteratorTest {
         // remove
     }
 }
+//喜当爹！
