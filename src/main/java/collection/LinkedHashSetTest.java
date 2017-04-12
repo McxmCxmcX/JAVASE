35cf4223782a6932f242c3dd08a899625dbc86c8
McxmCxmcX <414550551@qq.com>
2017-04-12 09:06:18 +0800

    update

--- src/main/java/collection/LinkedHashSetTest.java
+++
@@ -1,9 +0,0 @@
-package collection;
-
-/**
- * Created by heishuai.china.cn
- * 2017/4/11 16:01
- * JAVASE
- */
-public class LinkedHashSetTest {
-}
--- src/main/java/collection/TreeSetTest.java
+++ src/main/java/collection/TreeSetTest.java
@@ -1,9 +1,29 @@
 package collection;
-
+import java.util.TreeSet;
 /**
  * Created by heishuai.china.cn
- * 2017/4/11 16:03
+ * 2017/4/12 9:04
  * JAVASE
  */
 public class TreeSetTest {
+    public static void main(String[] args) {
+        TreeSet<String> strings = new TreeSet<>();
+        strings.add("d");
+        strings.add("c");
+        strings.add("b");
+        strings.add("a");
+        strings.add("一");
+        strings.add("二");
+        strings.add("三");
+        strings.add("四");
+
+        System.out.println((int) '一');
+        System.out.println((int) '二');
+        System.out.println((int) '三');
+        System.out.println((int) '四');
+
+        System.out.println(strings.size());
+        for (String string : strings) {
+        }
+    }
 }
