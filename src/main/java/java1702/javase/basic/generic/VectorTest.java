12e973c380507828b3b6e58cc9055b272a406514
McxmCxmcX <414550551@qq.com>
2017-04-13 10:05:06 +0800

    update

---
+++ src/main/java/java1702/javase/basic/generic/VectorTest.java
@@ -0,0 +1,21 @@
+package java1702.javase.basic.generic;
+
+import java.util.Vector;
+
+/**
+ * Created by heishuai.china.cn
+ * 2017/4/13 10:01
+ * JAVASE
+ */
+public class VectorTest {
+    public static void main(String[] args) {
+        Vector<String> vector = new Vector<>();//兼容
+        vector.add("test...");
+        vector.add("123");
+        vector.add("true");
+        vector.add("456");
+
+        String s = vector.get(3);// Type safe
+        System.out.println(s);
+    }
+}
