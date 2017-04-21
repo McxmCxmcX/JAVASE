26921c9a4bd39d8556d1792072238994304d7c44
McxmCxmcX <414550551@qq.com>
2017-04-21 20:23:10 +0800

    update

--- src/main/java/java1702/javase/basic/io/DataTest.java
+++ src/main/java/java1702/javase/basic/io/DataTest.java
@@ -1,9 +1,18 @@
 package java1702.javase.basic.io;
-
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.RandomAccessFile;
 /**
  * Created by heishuai.china.cn
  * 2017/4/21 20:07
  * JAVASE
  */
 public class DataTest {
+    public static void main(String[] args) {
+        DataInputStream dataInputStream = new DataInputStream(System.in);
+        DataOutputStream dataOutputStream = new DataOutputStream(System.out);
+//        dataInputStream.
+//        dataOutputStream.
+//        RandomAccessFile randomAccessFile
+    }
 }
