a2ced524a2f790c897dc7ceb717e2ecae216a143
McxmCxmcX <414550551@qq.com>
2017-04-21 20:24:07 +0800

    update

--- src/main/java/java1702/javase/basic/io/BufferedReaderTest.java
+++ src/main/java/java1702/javase/basic/io/BufferedReaderTest.java
@@ -1,9 +1,21 @@
 package java1702.javase.basic.io;
-
+import java.io.*;
 /**
  * Created by heishuai.china.cn
  * 2017/4/21 20:07
  * JAVASE
  */
 public class BufferedReaderTest {
+    private static final String FILE = "src/main/java/java1702/javase/io/BufferedReaderTest.java";
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
