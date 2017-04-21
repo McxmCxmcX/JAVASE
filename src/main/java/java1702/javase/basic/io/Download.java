e01dfa8b73ca58cd4775a695ddb6c1814bd4dc68
McxmCxmcX <414550551@qq.com>
2017-04-21 20:22:40 +0800

    update

--- src/main/java/java1702/javase/basic/io/Download.java
+++ src/main/java/java1702/javase/basic/io/Download.java
@@ -1,9 +1,35 @@
 package java1702.javase.basic.io;
-
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 /**
  * Created by heishuai.china.cn
  * 2017/4/21 20:07
  * JAVASE
  */
 public class Download {
+    private static final String IMAGE_URL = "http://s.cn.bing.net/th?id=OJ.58V1JdyCq3tEwg&pid=MSNJVFeeds/1.2.tif";
+
+    public static void main(String[] args) {
+        try {
+            URL url = new URL(IMAGE_URL);
+            try (
+                    InputStream inputStream = url.openStream();
+                    OutputStream outputStream = new FileOutputStream("test.png")
+            ) {
+                System.out.println(inputStream.available());
+                int i;
+                while ((i = inputStream.read()) != -1) {
+                    outputStream.write(i);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
 }
