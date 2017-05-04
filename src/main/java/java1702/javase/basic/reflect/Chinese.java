167d120c7ba20f3d319a1f1195ccd2beb72ed09e
McxmCxmcX <414550551@qq.com>
2017-05-04 09:05:40 +0800

    update

---
+++ src/main/java/java1702/javase/basic/reflect/Chinese.java
@@ -0,0 +1,27 @@
+//package java1702.javase.basic.reflect;
+//import java1702.javase.basic.Human;
+/**
+ * Created by heishuai.china.cn
+ * 2017/5/3 17:44
+ * JAVASE
+ */
+//public class Chinese extends  Human {
+//    private int i;
+//
+//    public Chinese(int i) {
+//        this.i=i;
+//    }
+//
+//    public void m(Shring s) {
+//        System.out.println(s);
+//    }
+//}
+//
+//class ChineseTest {
+//    public static void main(String[] args) throws ClassNotFoundException {
+//        Chinese chinese = new Chinese(1);
+//        class<Chinese> chineseClass1 = Chinese.class;
+//        class chineseclass2 = chinese.getClass();
+//        Class chineseClass3 = Class.forName("java1702.javase.reflect.Chinese")
+//    }
+//}
---
+++ src/main/java/java1702/javase/basic/reflect/Test.java
@@ -0,0 +1,12 @@
+package java1702.javase.basic.reflect;
+
+/**
+ * Created by heishuai.china.cn
+ * 2017/5/3 16:30
+ * JAVASE
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
